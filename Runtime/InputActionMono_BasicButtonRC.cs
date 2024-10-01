3bf250fc6af0d9d13bf44d370a07c61010141edd
Eloi Strée <eloistree@gmail.com>
2024-10-01 05:25:26 +0200

    Save: 10/01/2024 5:25 AM

---
+++ Runtime/InputActionMono_BasicButtonRC.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class InputActionMono_BasicButtonRC : MonoBehaviour {
+
+    public UnityEvent<bool> m_onButtonOn;
+    public InputActionReference m_buttonReference;
+
+    public bool m_valueIsOn;
+
+
+    public void OnEnable()
+    {
+        if (m_buttonReference == null)
+            return;
+        m_buttonReference.action.Enable();
+        m_buttonReference.action.performed += OnButton;
+        m_buttonReference.action.canceled += OnButton;
+    }
+    public void OnDisable()
+    {
+        if (m_buttonReference == null)
+            return;
+        m_buttonReference.action.performed -= OnButton;
+        m_buttonReference.action.canceled -= OnButton;
+    }
+
+    private void OnButton(InputAction.CallbackContext context)
+    {
+        bool value = context.ReadValue<float>() > 0.5f;
+        if (value != m_valueIsOn)
+        {
+            m_valueIsOn = value;
+            m_onButtonOn.Invoke(value);
+        }
+    }
+}
---
+++ Runtime/InputActionMono_JoystickToFloat2D.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class InputActionMono_JoystickToFloat2D : MonoBehaviour { 
+
+    public UnityEvent<float> m_onJoystickX;
+    public UnityEvent<float> m_onJoystickY;
+    public InputActionReference m_joystickReference;
+
+    public float m_joystickXValue = 0;
+    public float m_joystickYValue = 0;
+    public void OnEnable()
+    {
+
+        if (m_joystickReference == null)
+            return;
+        m_joystickReference.action.Enable();
+        m_joystickReference.action.performed += OnJoystick;
+        m_joystickReference.action.canceled += OnJoystick;
+    }
+
+    public void OnDisable()
+    {
+
+        if (m_joystickReference == null)
+            return;
+        m_joystickReference.action.performed -= OnJoystick;
+        m_joystickReference.action.canceled -= OnJoystick;
+    }
+
+    private void OnJoystick(InputAction.CallbackContext context)
+    {
+        Vector2 value = context.ReadValue<Vector2>();
+        if (value.x != m_joystickXValue)
+        {
+            m_joystickXValue = value.x;
+            m_onJoystickX.Invoke(value.x);
+        }
+        if (value.y != m_joystickYValue)
+        {
+            m_joystickYValue = value.y;
+            m_onJoystickY.Invoke(value.y);
+        }
+    }
+    
+}
